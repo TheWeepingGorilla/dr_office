e35c209970a3ecd0f41f6d6e7c9e847892e7fde1
Sean Morrison & Diane Douglas <student@epicodus.com>
2014-08-05 10:31:58 -0700

    Create doctor class with methods to save to the database, return all doctors from the database and compare two doctors.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,4 +6,27 @@ class Doctor
     @name = name
     @specialty = specialty
   end
+
+  def save
+    DB.exec("INSERT INTO doctors (name,specialty) VALUES ('#{@name}','#{@specialty}');")
+  end
+
+  def self.all
+    doctors = []
+    results = DB.exec("SELECT * FROM doctors;")
+    results.each do |doctor|
+      current_name = doctor["name"]
+      current_specialty = doctor["specialty"]
+      current_doctor = Doctor.new(current_name, current_specialty)
+      doctors << current_doctor
+    end
+    doctors
+  end
+
+  def ==(another_doctor)
+    (@name == another_doctor.name) && (@specialty == another_doctor.specialty)
+  end
+
 end
+
+
--- spec/dr_office_spec.rb
+++ spec/dr_office_spec.rb
@@ -2,6 +2,14 @@ require 'rspec'
 require 'pg'
 require 'doctor'
 
+DB = PG.connect({:dbname => 'doctors_office_tools'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM doctors *;")
+  end
+end
+
 describe Doctor do
 
   it 'is initialized with a name and specialty' do
@@ -14,4 +22,30 @@ describe Doctor do
     expect(test_doctor.name).to eq 'Dr. Mac'
     expect(test_doctor.specialty).to eq 'Ruby'
   end
+
+  it 'starts with no doctors' do
+    expect(Doctor.all).to eq []
+  end
+
+  it 'saves the doctor object to the database' do
+    test_doctor = Doctor.new('Dr. Mac', 'Ruby')
+    test_doctor.save
+    expect(Doctor.all).to eq [test_doctor]
+  end
+
+  it 'returns true if the given doctor object has the same name as the current doctor object.' do
+    test_doctor1 = Doctor.new('Dr. Mac', 'Ruby')
+    test_doctor2 = Doctor.new('Dr. Mac', 'Ruby')
+    expect(test_doctor1).to eq test_doctor2
+  end
+
+  it 'returns false if the given doctor object does not have the same name as the current doctor object.' do
+    test_doctor1 = Doctor.new('Dr. Mac', 'Ruby')
+    test_doctor2 = Doctor.new('Dr. Mac', 'JS')
+    test_doctor1.should_not eq test_doctor2
+  end
+
+
+
 end
+
