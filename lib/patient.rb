d397be081eca861eb5accfe10ee51e17addee5a4
Sean Morrison & Diane Douglas <student@epicodus.com>
2014-08-05 11:14:57 -0700

    Add save, query and equals methods to patient class.

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,4 +6,25 @@ class Patient
     @birthday = birthday
   end
 
+  def self.all
+    patients = []
+    results = DB.exec("SELECT * FROM patients;")
+    results.each do |patient|
+      current_name = patient["name"]
+      current_birthday = patient["birthday"]
+      formatted_birthday = current_birthday.split(" ")[0]
+      current_patient = Patient.new(current_name, formatted_birthday)
+      patients << current_patient
+    end
+    patients
+  end
+
+  def save
+    DB.exec("INSERT INTO patients (name, birthday) VALUES ('#{@name}', '#{@birthday}');")
+  end
+
+  def ==(another_patient)
+    (@name == another_patient.name) && (@birthday == another_patient.birthday)
+  end
+
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -8,7 +8,7 @@ DB = PG.connect({:dbname => 'doctors_office_tools'})
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec("DELETE FROM doctors *;")
+    DB.exec("DELETE FROM patients *;")
   end
 end
 
@@ -21,5 +21,19 @@ describe Patient do
   it 'returns the name and birthday of the patient' do
     expect(test_patient.name).to eq 'Bill Murray'
     expect(test_patient.birthday).to eq '1999-01-08'
+  end
+
+  it 'starts with no patients' do
+    expect(Patient.all).to eq []
+  end
+
+  it 'saves a patient to the database' do
+    test_patient.save
+    expect(Patient.all).to eq [test_patient]
+  end
+
+  it 'returns true if two patients have the same name and birthday' do
+    test_patient2 = Patient.new('Bill Murray', '1999-01-08')
+    expect(test_patient).to eq test_patient2
   end
 end
